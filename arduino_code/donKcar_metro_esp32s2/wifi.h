2905b2bf5d9453c11584bbfd2d72835cb8d47bd5
Don Korte <dnkorte@gmail.com>
2023-05-11 10:52:42 -0400

    moved arduino code to cleaner folders

--- arduino_code/donKcar_metro_esp32s2/wifi.h
+++ arduino_code/donKcar_metro_esp32s2/wifi.h
@@ -15,6 +15,7 @@
  * furnished to do so, subject to the following conditions:
  * 
  * The above copyright notice and this permission notice shall be included in all
+ * 0000000
  * copies or substantial portions of the Software.
  * 
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
